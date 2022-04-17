9792a3787a97ee3c09c7f95b3fc107cf1e30b356
“Jane <ttran161@ucsc.edu>
2022-04-17 10:43:30 -0700

    update

--- src/main.js
+++ src/main.js
@@ -25,6 +25,6 @@ let keyF, keyR, keyLEFT, keyRIGHT;
 // Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (60)
 // -> Redesign to cat catching food theme, sound change to cat meow, pastel aesthetic
 // Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (20)
-// -> Create a mice type that is smaller, moves faster, and worth 50 points
+// -> Create a mice type that is smaller, moves faster, and worth 40 points
 // Create new artwork for all of the in-game assets (rocket, spaceships, explosion) (20)
 // -> rocket = paw, spaceship = fish/mice, explosion = fish bone
